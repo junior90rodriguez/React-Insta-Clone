02a3c33006976a22a0337cea9e8cb32ff0235734
Junior <junior.arodriguez@icloud.com>
2019-06-04 22:38:11 -0500

    added SearchBar.css

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,26 @@
 import React from 'react';
+
 import IgLogo from '../images/iglogo.svg';
+import IgWord from '../images/igword.svg';
+import Compass from '../images/compass.png';
+import Heart from '../images/heart.png';
+import Person from '../images/person.png';
 
 function SearchBar() {
     return(
         <div className="searchBarContainer">
-            <img src={IgLogo}/>
+            <div className="logo">
+                <img src={IgLogo}/>
+                <img src={IgWord}/>  
+            </div>
+                <form>
+                    <input type='text' placeholder='Search' />  
+                </form> 
+            <div className="icons">
+                <img src={Compass} />
+                <img src={Heart} />
+                <img src={Person} />
+            </div>
         </div>
     )
 }
